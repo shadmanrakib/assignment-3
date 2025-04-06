72f841cb586f660a3086282122ab4e6822003e0d
shadmanrakib <srakibnyc1@gmail.com>
2025-04-06 12:35:41 -0400

    styled the credit list

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -6,20 +6,25 @@ Note: You need to work on this file for the Assignment.
 ==================================================*/
 import { Link } from "react-router-dom";
 import { formatDateStr, roundToHundredths } from "../utils";
+import styles from "./Credits.module.css";
 
 const Credits = ({ credits }) => {
   return (
     <div>
       <h1>Credits</h1>
       <br />
-      {credits.map((credit) => (
-        <div id={credit.id}>
-          <p>ID: {credit.id}</p>
-          <p>Amount: {roundToHundredths(credit.amount)}</p>
-          <p>Description: {credit.description}</p>
-          <p>Date: {formatDateStr(credit.date)}</p>
-        </div>
-      ))}
+      <div className={styles.creditList}>
+        {credits.map((credit) => (
+          <div id={credit.id} className={styles.creditItem}>
+            <p>ID: {credit.id}</p>
+            <p className={styles.creditAmount}>
+              ${roundToHundredths(credit.amount)}
+            </p>
+            <p>Description: {credit.description}</p>
+            <p>Date: {formatDateStr(credit.date)}</p>
+          </div>
+        ))}
+      </div>
       <br />
       <Link to="/">Return to Home</Link>
     </div>
