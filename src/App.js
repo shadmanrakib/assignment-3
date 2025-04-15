9acfb7fbb73861ba0d60116d27d7f89067f76fd0
bobawanna <evasea85@gmail.com>
2025-04-14 23:35:19 -0400

    Added Add Debit logic, fixed the discrepancies in styling/architecture overall

--- src/App.js
+++ src/App.js
@@ -75,6 +75,21 @@ class App extends Component {
     this.setState({ creditList: newCreditList, accountBalance: newBalance });
   };
 
+  //addDebit story
+  addDebit = ({ amount, description }) => {
+    const id = crypto.randomUUID();
+    const dateStr = new Date().toISOString().slice(0, 10); 
+    const newDebit = {
+      id,
+      date: dateStr,
+      amount: parseFloat(amount), 
+      description,
+    };
+    const newDebitList = [...this.state.debitList, newDebit];
+    const newBalance = this.state.accountBalance - amount;  // subtract debit from balance
+    this.setState({ debitList: newDebitList, accountBalance: newBalance });
+  };
+
   // Create Routes and React elements to be rendered using React components
   render() {
     // Create React elements and pass input props to components
@@ -103,6 +118,7 @@ class App extends Component {
     const DebitsComponent = () => (
       <Debits
         debits={this.state.debitList}
+        addDebit={this.addDebit}
         accountBalance={this.state.accountBalance}
       />
     );
---
+++ src/components/AddDebitForm.js
@@ -0,0 +1,48 @@
+import React, { useMemo, useState } from "react";
+
+const AddDebitForm = (props) => {
+  const [description, setDescription] = useState("");
+  const [amount, setAmount] = useState(0);
+
+  const disableButton = useMemo(
+    () => amount <= 0 || description.trim() === "",
+    [amount, description]
+  );
+
+  const onSubmit = () => {
+    props.addDebit({ amount, description });
+    setAmount(0);
+    setDescription("");
+  };
+
+  return (
+    <div>
+      <p>Add Debit</p>
+
+      <div>
+        <label htmlFor="description">Description</label>
+        <input
+          type="text"
+          id="description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="amount">Amount</label>
+        <input
+          type="number"
+          id="amount"
+          value={amount}
+          onChange={(e) => setAmount(Number.parseFloat(e.target.value))}
+        />
+      </div>
+
+      <button type="button" onClick={onSubmit} disabled={disableButton}>
+        Add Debit
+      </button>
+    </div>
+  );
+};
+
+export default AddDebitForm;
