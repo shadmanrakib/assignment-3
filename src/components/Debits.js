8f23f5dcf54e37a40c6b1956cb43447e8ee77af0
Ahmed Said <ahmedsaid@mac.mynetworksettings.com>
2025-04-14 23:55:24 -0400

    debit.js

--- src/components/Debits.js
+++ src/components/Debits.js
@@ -1,40 +1,34 @@
-/*==================================================
-src/components/Debits.js
 
-The Debits component contains information for Debits page view.
-Note: You need to work on this file for the Assignment.
-==================================================*/
 import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
+import AddDebitForm from "./AddDebitForm";
+import { formatDateStr, roundToHundredths } from "../utils";
+import styles from "./Debits.module.css";
 
-const Debits = (props) => {
-  // Create the list of Debit items
-  let debitsView = () => {
-    const { debits } = props;
-    return debits.map((debit) => {  // Extract "id", "amount", "description" and "date" properties of each debits JSON array element
-      let date = debit.date.slice(0,10);
-      return <li key={debit.id}>{debit.amount} {debit.description} {date}</li>
-    });
-  }
-  // Render the list of Debit items and a form to input new Debit item
-  return (
-    <div>
-      <h1>Debits</h1>
+const Debits = ({ debits, addDebit, accountBalance }) => {
+return (
+<div>
+<h1>Debits</h1>
+<div>
+<h3>Account Balance: ${roundToHundredths(accountBalance)}</h3>
+</div>
 
-      {debitsView()}
-
-      <form onSubmit={props.addDebit}>
-        <label htmlFor="description"> Description</label>
-        <input type="text" name="description" />
-
-        <label htmlFor="amount"> Amount</label>
-        <input type="number" name="amount" />
-
-        <button type="submit">Add Debit</button>
-      </form>
-      <br/>
-      <Link to="/">Return to Home</Link>
-    </div>
-  );
-}
-
+<div className={styles.debitList}>
+{debits.map((debit) => (
+<div key={debit.id} id={debit.id} className={styles.debitItem}>
+<p className={styles.debitAmount}>
+${roundToHundredths(debit.amount)}
+</p>
+<p>Description: {debit.description}</p>
+<p>Date: {formatDateStr(debit.date)}</p>
+</div>
+))}
+</div>
+<br />
+<AddDebitForm addDebit={addDebit} />
+<br />
+<Link to="/">Return to Home</Link>
+</div>
+);
+};
 export default Debits;
